7fdee1542b8ca796d726fb8c66ab6de3182c0429
Alexey <redy22@mail.ru>
2022-11-27 02:25:26 +0400

    new4

--- Program.cs
+++ Program.cs
@@ -1,73 +1,74 @@
-﻿// Задание №1
+﻿Задание №1
 
-// Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите первое число: ");
 
-// int num1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("Введи второе число: ");
+Console.WriteLine("Введи второе число: ");
 
-// int num2 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-// if (num1 > num2) 
-// {
-//     Console.WriteLine($"Число {num1} больше, чем {num2}");
-// }
+if (num1 > num2) 
+{
+    Console.WriteLine($"Число {num1} больше, чем {num2}");
+}
 
-// else
-// {
-//     Console.WriteLine($"Число {num2} больше, чем {num1}");
-// }
-
-
+else
+{
+    Console.WriteLine($"Число {num2} больше, чем {num1}");
+}
 
 
-// Console.WriteLine("Введите первое число: ");
+Задание №2
 
-// int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
 
-// Console.WriteLine("Введите второе число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-// int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
 
-// Console.WriteLine("Введите третье число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-// int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
 
-// int max = num1;
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-// if (num2>max)
-//     max = num2;
+int max = num1;
 
-// if (num3>max);
-//     max=num3;
+if (num2>max)
+    max = num2;
 
-// Console.WriteLine($"Максимальное из введенных чисел = {max}");
+if (num3>max);
+    max=num3;
+
+Console.WriteLine($"Максимальное из введенных чисел = {max}");
 
 
+Задание №3
 
+int a;
 
-// int a;
+Console.WriteLine("Введите число: ");
 
-// Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine()); 
 
-// a = Convert.ToInt32(Console.ReadLine()); 
+if (a % 2 == 0)
 
-// if (a % 2 == 0)
+{
 
-// {
+Console.WriteLine("Четное число");
 
-// Console.WriteLine("Четное число");
+}
 
-// }
+else
 
-// else
+{
 
-// {
+Console.WriteLine("Нечетное");
 
-// Console.WriteLine("Нечетное");
+}
 
-// }
-
+Задание №4
 
 Console.WriteLine("Введите число: ");
 
