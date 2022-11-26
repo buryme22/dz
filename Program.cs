bad1293a480e70daefff86b877c55abbdbcef21f
Alexey <redy22@mail.ru>
2022-11-27 01:28:23 +0400

    new

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи второе число: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+if (num1 > num2) 
+{
+    Console.WriteLine($"Число {num1} больше, чем {num2}");
+}
+
+else
+{
+    Console.WriteLine($"Число {num2} больше, чем {num1}");
+}
